08e3235899b7e0e370821a47868038f445047494
Hitesh Singh solanki <117797208+CodeVersePro@users.noreply.github.com>
2022-11-17 09:46:02 +0530

    update

--- 2. Production Grade/1.function/myfunc.py
+++ 2. Production Grade/1.function/myfunc.py
@@ -12,3 +12,17 @@ def floor_dev(a,b):
 
 def power(a,b):
     return a**b
+
+def letterlist(string):
+
+    list1 = []
+
+    for letter in string:
+
+        if letter == ' ':
+            pass
+        else:
+            list1.append(letter)
+
+    return list1
+
