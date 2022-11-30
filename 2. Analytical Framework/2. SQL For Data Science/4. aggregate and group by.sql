514a6bc197464e9c67c3a1949da6c6008abf5aea
codeversepro <codeversepro@gmail.com>
2022-11-30 21:45:55 +0530

    vs code

---
+++ 2. Analytical Framework/2. SQL For Data Science/4. aggregate and group by.sql
@@ -0,0 +1,47 @@
+/* using like and ilike with wild card % and _*/
+
+SELECT * from city
+
+/* like is case sensetive*/
+
+SELECT city FROM city
+WHERE city LIKE 'b%'
+
+/* i like is not case sensitive*/
+
+SELECT city FROM city
+WHERE city LIKE 'B%'
+
+/*using wild card __ to specify length */
+
+SELECT city FROM city
+WHERE city LIKE 'B___'
+
+SELECT city FROM city
+WHERE city LIKE '%h%'
+
+SELECT * from customer
+
+SELECT * FROM customer
+WHERE first_name ilike 'a%' and last_name ilike 'a%'
+
+
+/* AGREGATION CALCULATION sum,min,max avg */
+
+SELECT * FROM film
+
+SELECT SUM(replacement_cost) FROM film
+
+SELECT MIN(replacement_cost) FROM film
+
+SELECT MAX(replacement_cost) from film
+
+SELECT AVG(replacement_cost) from film
+
+/* Rounding off*/
+
+SELECT round(avg(replacement_cost),2) FROM film
+
+SELECT MAX(replacement_cost), MIN(replacement_cost) FROM film
+
+SELECT SUM(replacement_cost) FROM film
