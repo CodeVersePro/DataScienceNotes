187e8f355b1f4240d3c339569c42525b26ba49b8
codeversepro <codeversepro@gmail.com>
2022-12-01 09:43:23 +0530

    vs code

--- 2. Analytical Framework/2. SQL For Data Science/4. aggregate and group by.sql
+++ 2. Analytical Framework/2. SQL For Data Science/4. aggregate and group by.sql
@@ -45,3 +45,51 @@ SELECT round(avg(replacement_cost),2) FROM film
 SELECT MAX(replacement_cost), MIN(replacement_cost) FROM film
 
 SELECT SUM(replacement_cost) FROM film
+
+
+/* group by */
+
+SELECT * FROM payment
+
+SELECT customer_id, SUM(amount) from payment
+GROUP BY customer_id
+ORDER BY sum(amount) ASC
+
+SELECT DATE(payment_date), SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC
+
+SELECT DATE(payment_date), COUNT(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC
+
+
+/* apply where statement in group by*/
+
+SELECT * FROM payment
+
+SELECT customer_id, SUM(amount) FROM payment /*apply where on not aggregate with where */
+WHERE customer_id IN (184,87,477)
+GROUP BY customer_id
+
+/* on applying ggregate */
+
+SELECT customer_id, SUM(amount) FROM payment 
+WHERE customer_id IN (184,87,477)
+GROUP BY customer_id
+HAVING sum(amount) > 100
+ORDER BY SUM(amount) DESC
+
+/* as query for aliasing columns */
+
+SELECT customer_id as CustomerID from payment
+
+SELECT count(*) as TOTALRAWS from payment
+
+SELECT customer_id as CID, amount as SPENt FROM payment /* for multiple raws */
+
+SELECT customer_id AS CustomID, SUM(amount) AS Total_Spent FROM payment 
+WHERE customer_id IN (184,87,477)
+GROUP BY customer_id
+HAVING sum(amount) > 100
+ORDER BY SUM(amount) DESC
