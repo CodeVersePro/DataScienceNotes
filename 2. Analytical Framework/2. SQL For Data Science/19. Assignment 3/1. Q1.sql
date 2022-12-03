de5dc9bb26b027abd45018542a5968ca1e2cc689
codeversepro <codeversepro@gmail.com>
2022-12-03 21:47:34 +0530

    vs code

--- 2. Analytical Framework/2. SQL For Data Science/19. Assignment 3/1. Q1.sql
+++ 2. Analytical Framework/2. SQL For Data Science/19. Assignment 3/1. Q1.sql
@@ -1,15 +1,57 @@
 
-show datestyle
-SEt datestyle to german
+/* insepecting some data */
 
-CREATE TABLE order_tb(
+select * from orders
 
-        id INTEGER,
-        user_id INTEGER,
-        total INTEGER,
-        created TIMESTAMP
-)
+/* lets find the minimum week according to creation date of user*/
+
+select userid,min(extract(week from created)) as min_week from orders
+GROUP BY userid
+ORDER BY userid
+
+/* storing week 0 date in TEMPORARY table */
+
+create TEMPORARY TABLE week0 as 
+select userid,min(extract(week from created)) as min_week from orders
+GROUP BY userid
+ORDER BY userid
 
 
-copy order_tb from '/home/codeversepro/Documents/DataScienceNotes/2. Analytical Framework/2. SQL For Data Science/19. Assignment 3/data/Q1.csv' with header
+SELECT * from week0
 
+/* now getting all week of user and storing in TEMPORARY table*/
+
+create TEMPORARY TABLE all_week as
+SELECT userid, extract(week from created) as login_week from orders
+GROUP BY userid,created
+ORDER BY userid,login_week
+
+SELECT * from all_week
+
+/* now lets calculate week number which is 0 for week 1 and so on*//
+
+create TEMPORARY TABLE week_id as
+SELECT all_week.userid, all_week.login_week, week0.min_week as first_week, (all_week.login_week - week0.min_week) as week_number from all_week
+INNER JOIN week0
+on all_week.userid = week0.userid
+ORDER BY userid, login_week
+
+ALTER TABLE week_id
+RENAME COLUMN min_week to first_week
+
+SELECT * from week_id
+ORDER BY first_week
+
+
+create TEMPORARY TABLE Ten_week_ret as
+SELECT first_week,
+
+    sum( CASE WHEN week_number =0 then 1 else 0 END) as week_0,
+    sum( CASE WHEN week_number =1 then 1 else 0 END) as week_1,
+    sum( CASE WHEN week_number =2 then 1 else 0 END) as week_2
+    
+from week_id
+GROUP BY (first_week)
+ORDER BY first_week
+
+SELECT * from ten_week_ret
